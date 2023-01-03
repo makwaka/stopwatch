0eb28e001b46ef207bda4e26bdc842a3ef8eb7be
Phariq <pharisnjuguna22@gmail.com>
2023-01-03 20:10:07 +0300

    Edited styles

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
 // select items
 
 const startStop = document.querySelector('#startStopBtn');
-
 const resetBtn = document.querySelector('#resetBtn');
 
 // variables for time
-
 let seconds = 0;
 let minutes = 0;
 let hours = 0;
@@ -15,14 +13,11 @@ let leadingSeconds = 0;
 let leadingMinutes = 0;
 let leadingHours = 0;
 
-
 // set interval and timer status
-
 let timerInterval = null;
 let timerStatus = 'stopped';
 
 // start watch function
-
 const stopWatch = () => {
     seconds++;
 
@@ -35,7 +30,6 @@ const stopWatch = () => {
             hours++;
         };
     };
-
         if(seconds < 10){
             leadingSeconds = '0' + seconds.toString();
         }else{
@@ -53,11 +47,8 @@ const stopWatch = () => {
         }
     let displayTimer = document.getElementById('timer').innerText = leadingHours + ':' + leadingMinutes + ':' + leadingSeconds;
 };
-
 // 1000ms = 1s
-
 // window.setInterval(stopWatch, 1000);
-
 startStop.addEventListener('click', () => {
 if(timerStatus === 'stopped'){
     timerInterval = window.setInterval(stopWatch, 1000);
@@ -69,13 +60,11 @@ if(timerStatus === 'stopped'){
     timerStatus =  'stopped';
 }
 });
-
 resetBtn.addEventListener('click', () => {
 
     window.clearInterval(timerInterval);
     seconds = 0;
     minutes = 0;
     hours = 0;
-
     document.getElementById('timer').innerHTML = '00:00:00';
 });
